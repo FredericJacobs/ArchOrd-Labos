75d0666511ea0d61dec364385bd394af3dbfde15
Romain Ruetschi <romain.ruetschi@gmail.com>
2013-10-21 15:24:35 +0200

    Use separate processes for read and write.

--- Lab_4-5-Memories/vhdl/RAM.vhd
+++ Lab_4-5-Memories/vhdl/RAM.vhd
@@ -19,29 +19,28 @@ end RAM;
 
 architecture synth of RAM is
     signal reg_read  : std_logic;
-    signal reg_write : std_logic;
     signal memory    : ram_array;
-	 signal saved_address : std_logic_vector(7 downto 0);
 begin
     process(clk, read, cs)
     begin
         if (rising_edge(clk)) then
             reg_read <= read and cs;
-            reg_write <= write and cs;
-				saved_address <= address(9 downto 2);
         end if;
     end process;
 
-    process(saved_address, reg_read, reg_write)
+    process(reg_read, address)
     begin
         rddata <= (others => 'Z');
-
         if (reg_read = '1') then
-            rddata <= memory(to_integer(unsigned(saved_address)));
-        elsif (reg_write = '1') then
-            memory(to_integer(unsigned(saved_address))) <= wrdata;
+            rddata <= memory(to_integer(unsigned(address(9 downto 2))));
         end if;
     end process;
 
+    process(write, wrdata, address)
+    begin
+        if (rising_edge(clk) and write = '1')
+            memory(to_integer(unsigned(address(9 downto 2)))) <= wrdata;
+        end if;
+    end process;
 
 end synth;
