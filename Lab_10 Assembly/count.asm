887db86e0a2f6a6d8ce95a6ed302a556d4b76f38
Romain Ruetschi <romain.ruetschi@gmail.com>
2013-11-26 23:24:13 +0100

    Fix a couple typos, and replace cmpltu/beq by bgeu.

--- Lab_10 Assembly/count.asm
+++ Lab_10 Assembly/count.asm
@@ -1,7 +1,7 @@
 
 main:                               ; :main
     addi a0, zero, data             ; a0: Array address
-    addi a1, zero, 5                ; a1: Number of elements
+    addi a1, zero, 8                ; a1: Number of elements
     call count                      ; proc()
     break                           ; end of the program
 
@@ -15,24 +15,27 @@ count:
 
 ; t4: current element, then its first bit
 loop:
-    cmpltu t3, t1, a1               ; t3 = (t1 < a1)
-    beq t3, zero, return            ; if (!t3) goto return
+    bgeu t1, a1, return             ; if (t1 >= a1) goto return
     ldw t4, 0(t0)                   ; t4 = mem[t0]
     srli t4, t4, 31                 ; t4 = first bit of t4
     xori t4, t4, 1                  ; t4 = 1 if t4 is positive, 0 otherwise
     add v0, v0, t4                  ; v0 = v0 + t4
 
 next:
-    add t1, t1, 1                   ; t1 = t1 + 1
-    add t0, t0, 4                   ; t0 = t0 + 4
+    addi t1, t1, 1                   ; t1 = t1 + 1
+    addi t0, t0, 4                   ; t0 = t0 + 4
     br loop
 
 return:
     ret
 
-.data:
+; 5 positive numbers, 8 in total
+data:
     .word 9
     .word 3
     .word -4
     .word 7
-    .word -2
+    .word 32
+	.word -2
+    .word -10
+    .word 42
