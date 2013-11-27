1a0c8a15dca1a02afece5d6b5f70e984711cec2c
Frederic Jacobs <github@fredericjacobs.com>
2013-11-27 11:37:31 +0100

    Fixing clear_leds

--- Lab_11 Pong/pong.asm
+++ Lab_11 Pong/pong.asm
@@ -5,6 +5,9 @@
 .equ BUTTONS, 0x2030 ; Buttons address
 
 clear_leds: Goal is to initialize all LEDs to 0
+;The LED array has a size of 96 bits, or 3 words of 32 bits.
 stw zero, LEDS(zero)
+stw zero, LEDS+8 (zero)
+stw zero, LEDS+16 (zero)
 
 setPixel: The set_pixel procedure takes two coordinates as arguments and turns on the corresponding pixel on the display.
