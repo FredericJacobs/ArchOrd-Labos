e00e66aac07f435d8369459b84ff77082922400f
Frederic Jacobs <github@fredericjacobs.com>
2013-11-27 11:48:15 +0100

    Font data.

--- Lab_11 Pong/pong.asm
+++ Lab_11 Pong/pong.asm
@@ -11,3 +11,23 @@ stw zero, LEDS+8 (zero)
 stw zero, LEDS+16 (zero)
 
 setPixel: The set_pixel procedure takes two coordinates as arguments and turns on the corresponding pixel on the display.
+
+font_data:
+.word 0x7E427E00 ; 0 
+.word 0x407E4400 ; 1 
+.word 0x4E4A7A00 ; 2 
+.word 0x7E4A4200 ; 3 
+.word 0x7E080E00 ; 4 
+.word 0x7A4A4E00 ; 5 
+.word 0x7A4A7E00 ; 6 
+.word 0x7E020600 ; 7 
+.word 0x7E4A7E00 ; 8 
+.word 0x7E4A4E00 ; 9 
+.word 0x7E127E00 ; A 
+.word 0x344A7E00 ; B 
+.word 0x42423C00 ; C 
+.word 0x3C427E00 ; D 
+.word 0x424A7E00 ; E 
+.word 0x020A7E00 ; F
+
+￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼
